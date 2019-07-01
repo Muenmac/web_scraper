f000b86fcf739f6b2b1875da6519f336c8e35769
Marc <muenmac@googlemail.com>
2019-07-01 13:24:50 +0200

    update on requirements, change of library (bs4-->selenium) in finance_data due to JavaScript content in parsed web page

--- finance_data.py
+++ finance_data.py
@@ -4,29 +4,44 @@
 
 # Import libs
 from bs4 import BeautifulSoup as BS
-import urllib.request as ul
+import requests as r
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # read data from Bloomberg Website
-
 source = "https://www.bloomberg.com/quote/SPX:IND"
+
+browser = webdriver.Firefox(executable_path='/usr/local/share/gecko_driver/geckodriver')
+
 try:
-    r = ul.urlopen(source)
-    print("successfully read")
+    browser.get(source)
+    print("success")
 except:
     print("WTF")
+textlist = browser.find_elements_by_tag_name("span")
+
+for text_elements in textlist:
+    text = text_elements.text
+    print(text)
+
+# delay = 3
+# WebDriverWait(browser, delay).until(EC.presence_of_element_located(browser.find_elements_by_id("a")))
 
 
 # Parse data from website
-soup = BS(r, "lxml")
-print(soup.prettify())
+# soup = BS(, "html.parser")
+# elements = soup.select("div > span")
+# print(soup.prettify())
+
 
 
 # --------end of basic web scraping---------
 # find finance data on Bloomberg website
 
-name_box = soup.find("h1", attrs={"class": "companyName__99a4824b"})
-name = name_box.text.strip()
-print(name)
+#name_box = soup.find("h1", attrs={"class": "companyName__99a4824b"})
+#name = name_box.text.strip()
+#print(name)
 
 # seems like the web page is not parsed properly since there is no companyname in xml view included.
