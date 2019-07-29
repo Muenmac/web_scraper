25dac9c146c6b00f6aa60858c7fb78fe5f65ef8d
Marc <muenmac@googlemail.com>
2019-07-29 15:04:55 +0200

    fixed DataFrame, added Query

--- scopus.py
+++ scopus.py
@@ -8,26 +8,16 @@ from pytictoc import TicToc
 t = TicToc()
 
 t.tic()
-s = ScopusSearch('TITLE-ABS ( energ*  OR  power )  AND  TITLE-ABS ( predict*  OR  forecast*  OR  prognos*  OR  model*'
-                 '  OR  consumption  OR  efficiency )  AND  TITLE-ABS ( manufactur*  OR  production )  '
-                 'AND  PUBYEAR  >  2008  AND  ( SUBJAREA ( engi )  OR  SUBJAREA ( ener ) )  AND NOT  KEY ( solar )  '
-                 'AND NOT  KEY ( wind )  AND NOT  KEY ( photovoltaic )', refresh = False)
+s = ScopusSearch('FIRSTAUTH(thiede s.) AND PUBYEAR AFT 2008 AND AUTHKEY(energ*)', refresh = True)
 
 t.toc('Query took ')
 
 t.tic()
-# pd.set_option('display.max_columns', None)
+
+pd.set_option('display.max_columns', None)
+
 df = pd.DataFrame(s.results)
 
 t.toc('Creating DataFrame took ')
 
-print(df.head(5))
-
-
-
-
-
-
-
-
-
+print(df.head())
