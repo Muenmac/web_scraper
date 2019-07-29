503cf22c90cc5b2bfc366db0279439bf3e3f9fdd
Marc <muenmac@googlemail.com>
2019-07-29 14:52:47 +0200

    created gitignore with gitignore.io (see details)

--- scopus.py
+++ scopus.py
@@ -3,20 +3,26 @@
 
 from pybliometrics.scopus import ScopusSearch
 import pandas as pd
-import TikTok
+from pytictoc import TicToc
 
+t = TicToc()
 
+t.tic()
 s = ScopusSearch('TITLE-ABS ( energ*  OR  power )  AND  TITLE-ABS ( predict*  OR  forecast*  OR  prognos*  OR  model*'
                  '  OR  consumption  OR  efficiency )  AND  TITLE-ABS ( manufactur*  OR  production )  '
                  'AND  PUBYEAR  >  2008  AND  ( SUBJAREA ( engi )  OR  SUBJAREA ( ener ) )  AND NOT  KEY ( solar )  '
                  'AND NOT  KEY ( wind )  AND NOT  KEY ( photovoltaic )', refresh = False)
 
+t.toc('Query took ')
 
+t.tic()
 # pd.set_option('display.max_columns', None)
 df = pd.DataFrame(s.results)
 
+t.toc('Creating DataFrame took ')
+
 print(df.head(5))
-# print(df.loc[:, ['eid', 'doi', 'title', 'subtype', 'affilname', 'authkeywords']])
+
 
 
 
