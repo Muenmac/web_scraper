ab597034094df7299378e234d7acdfefb1d0591a
Marc <muenmac@googlemail.com>
2019-07-29 14:24:27 +0200

    Added Query engine for Scopus, retreives some basic data for search query

--- scopus.py
+++ scopus.py
@@ -3,15 +3,20 @@
 
 from pybliometrics.scopus import ScopusSearch
 import pandas as pd
+import TikTok
 
 
-s = ScopusSearch('FIRSTAUTH(thiede, s.)', refresh = True)
+s = ScopusSearch('TITLE-ABS ( energ*  OR  power )  AND  TITLE-ABS ( predict*  OR  forecast*  OR  prognos*  OR  model*'
+                 '  OR  consumption  OR  efficiency )  AND  TITLE-ABS ( manufactur*  OR  production )  '
+                 'AND  PUBYEAR  >  2008  AND  ( SUBJAREA ( engi )  OR  SUBJAREA ( ener ) )  AND NOT  KEY ( solar )  '
+                 'AND NOT  KEY ( wind )  AND NOT  KEY ( photovoltaic )', refresh = False)
 
 
-pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_columns', None)
 df = pd.DataFrame(s.results)
 
-print(df.loc[:, ['eid', 'doi', 'title', 'subtype', 'affilname', 'authkeywords']])
+print(df.head(5))
+# print(df.loc[:, ['eid', 'doi', 'title', 'subtype', 'affilname', 'authkeywords']])
 
 
 
