e4d1216a4583cc52f3a99ae7b36571ff1826e778
Marc <muenmac@googlemail.com>
2019-08-01 15:28:40 +0200

    some changes

--- read_csv.py
+++ read_csv.py
@@ -8,7 +8,8 @@ link = "/home/marc/PycharmProjects/web scraper/scopus.csv"
 csv = pd.read_csv(link)
 df = pd.DataFrame(csv)
 
-df.reindex(['Authors', 'Title', 'Year', 'Abstract', 'DOI', 'Link', 'Author Keywords', 'Index Keywords', 'References'])
-df2 = df.rename(columns={"Title": "tittel"})
 
-print(df2.columns, df2.shape, df2.info, df2.count)
+df.reindex(['Authors', 'Title', 'Year', 'Abstract', 'DOI', 'Link', 'Author Keywords', 'Index Keywords', 'References'])
+
+
+
