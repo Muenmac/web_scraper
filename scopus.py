111c47cf029082dfb59983bff3021ec888a1b536
Marc <muenmac@googlemail.com>
2019-08-02 13:59:57 +0200

    definition of function call + pickle dump of results

--- scopus.py
+++ scopus.py
@@ -5,19 +5,25 @@ from pybliometrics.scopus import ScopusSearch
 import pandas as pd
 from pytictoc import TicToc
 
+# Initialize TicToc for performance measures
 t = TicToc()
 
-t.tic()
-s = ScopusSearch('FIRSTAUTH(thiede s.) AND PUBYEAR AFT 2008 AND AUTHKEY(energ*)', refresh = True)
 
-t.toc('Query took ')
+def execute_search(name):
+    """Execute a search on Scopus using Scopus Query Language and print brief results"""
+    t.tic()
+    s = ScopusSearch('TITLE-ABS ( energ*  OR  power  OR  load )  AND  '
+                     'TITLE-ABS ( predict*  OR  forecast*  OR  prognos*  OR  model*  OR  consumption  OR  efficiency ) '
+                     'AND  TITLE-ABS ( manufactur*  OR  production )  AND  PUBYEAR  >  2000  AND  SUBJAREA ( engi )  '
+                     'OR  SUBJAREA ( ener )  AND  KEY ( energ* )  AND  ( KEY ( predic* )  OR  KEY ( forecast* )  '
+                     'OR  KEY ( prognos* ) )  AND NOT  KEY ( solar* )  AND NOT  KEY ( wind* )  '
+                     'AND NOT  KEY ( photovoltaic* )  AND NOT  KEY ( bio* )  AND NOT  KEY ( *hydro* )  '
+                     'AND NOT  KEY ( *gas* )')
 
-t.tic()
+    query_results = pd.DataFrame(s.results)
+    # print(query_results.shape())
 
-pd.set_option('display.max_columns', None)
-
-df = pd.DataFrame(s.results)
-
-t.toc('Creating DataFrame took ')
-
-print(df.head())
+    # Select name for pickle data
+    # name = 'query_results_LCE2020_test'
+    query_results.to_pickle('./Scopus_dumps' + name + '.pkl')
+    t.toc('Query and Saving DataFrame took ')
