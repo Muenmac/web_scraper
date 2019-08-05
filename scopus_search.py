aa93d967218e7850c08d7351ef88e3ef3ecb4ee4
Marc <muenmac@googlemail.com>
2019-08-05 17:10:11 +0200

    some changes

--- scopus_search.py
+++ scopus_search.py
@@ -7,12 +7,7 @@ from pytictoc import TicToc
 
 # Initialize TicToc for performance measures
 t = TicToc()
-
-
-def execute_search(dump_name):
-    """Execute a search on Scopus using Scopus Query Language and print brief results"""
-    t.tic()
-    s = ScopusSearch('TITLE-ABS ( energ*  OR  power  OR  load )  AND  '
+query = ('TITLE-ABS ( energ*  OR  power  OR  load )  AND  '
                      'TITLE-ABS ( predict*  OR  forecast*  OR  prognos*  OR  model*  OR  consumption  OR  efficiency ) '
                      'AND  TITLE-ABS ( manufactur*  OR  production )  AND  PUBYEAR  >  2000  AND  SUBJAREA ( engi )  '
                      'OR  SUBJAREA ( ener )  AND  KEY ( energ* )  AND  ( KEY ( predic* )  OR  KEY ( forecast* )  '
@@ -20,16 +15,20 @@ def execute_search(dump_name):
                      'AND NOT  KEY ( photovoltaic* )  AND NOT  KEY ( bio* )  AND NOT  KEY ( *hydro* )  '
                      'AND NOT  KEY ( *gas* )')
 
-    query_results = pd.DataFrame(s.results)
-    # print(query_results.shape())
+def execute_search(dump_name, query):
+    """Execute a search on Scopus using Scopus Query Language and print brief results"""
+    t.tic()
+    res = ScopusSearch(query)
+
+    query_res = pd.DataFrame(res.results)
 
     # Select name for pickle data
-    query_results.to_pickle('./Scopus_dumps/' + dump_name + '.pkl')
-    t.toc('Query and Saving DataFrame took ')
+    query_res.to_pickle('./Scopus_dumps/' + dump_name + '.pkl')
+    t.toc('Query and saving DataFrame took ')
 
 
-def load_pickle():
-    """Reload pickled data and manipulate search results"""
-    results = pd.read_pickle('./Scopus_dumps/query_results_LCE2020.pkl')
-    print(results.head(), results.info())
+def load_pickle(res_pickle):
+    """Reload pickled data and manipulate search results, res_pickle = Name of pickle data in Scopus_dumps folder"""
+    results = pd.read_pickle('./Scopus_dumps/'+res_pickle)
+    # print(results.head(), results.info())
 
