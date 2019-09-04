e63098f63fc3f3babcc51bd569b75a66d05a040f
Marc <muenmac@googlemail.com>
2019-09-04 07:23:02 +0200

    some changes

--- manipulate_export.py
+++ manipulate_export.py
@@ -5,20 +5,13 @@ import pandas as pd
 import numpy as np
 
 try:
-    res = pd.read_pickle('/home/marc/PycharmProjects/web scraper/Scopus_dumps/query_results_LCE2020.pkl')
+    res_np = np.load('/home/marc/PycharmProjects/web scraper/Scopus_dumps/query_results_LCE2020.pkl', allow_pickle=True)
+    res_pd = pd.read_pickle('/home/marc/PycharmProjects/web scraper/Scopus_dumps/query_results_LCE2020.pkl')
 except:
     print("Reading data failed")
 
 
-
-
-
-
-
-
-
-
-
+res_np.head()
 
 
 
